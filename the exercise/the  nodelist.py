e9817279438c7651e82a0fc6ef7ead28cb870403
ThinkingBox <dei2959558280@qq.com>
2026-02-04 09:47:38 +0800

    第一次提交：创建蓝桥杯2026练习项目，包含练习和学习

--- the exercise/the  nodelist.py
+++ the exercise/the  nodelist.py
@@ -1,21 +1,44 @@
-class Nodelist:
+class Node:
     def __init__(self,val,next=None):
         self.val=val
         self.next=next
-class linkedlist:
-    def __init__(self,head=None):
-        self.head=head
-    def append(self,val):
-        new_node=Nodelist(val)
+class linkedNode:
+    def __init__(self,head):
+        self.head=None
+    def prend(self,val):
+        node=Node(val)
         if self.head==None:
-            self.head=new_node
+            self.head=node
+            return
         else:
             current=self.head
             while current.next:
                 current=current.next
-            current.next=new_node
-    def preappend(self,val):
-        new_node=Nodelist(val)
-        new_node.next=self.head
-        self.head=new_node
+            current.next=node
+    def preprend(self,val):
+        node=Node(val)
+        node.next=self.head
+        self.head=node
+    def inser(self,where,val):
+        node=Node(val)
+        if self.head==None:
+            self.head=node
+            return
+        else:
+            current=self.head
+            for i in range(where-1):
+                current=current.next
+            node.next=current.next
+            current.next=node
+
+
+
+
+
+
+
+
+
+
+
 
