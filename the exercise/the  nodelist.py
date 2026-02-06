0f3285d62d6034ebfb96b34974b03f1cdd49d126
ThinkingBox <dei2959558280@qq.com>
2026-02-06 09:15:25 +0800

    第一次提交：创建蓝桥杯2026练习项目，包含练习和学习

--- the exercise/the  nodelist.py
+++ the exercise/the  nodelist.py
@@ -1,11 +1,11 @@
-class Node:
+class Node():
     def __init__(self,val,next=None):
         self.val=val
         self.next=next
-class linkedNode:
-    def __init__(self,head):
-        self.head=None
-    def prend(self,val):
+class Linkednode():
+    def __init__(self,head=None):
+        self.head=head
+    def append(self,val):
         node=Node(val)
         if self.head==None:
             self.head=node
@@ -15,11 +15,16 @@ class linkedNode:
             while current.next:
                 current=current.next
             current.next=node
-    def preprend(self,val):
+            return
+    def preappend(self,val):
         node=Node(val)
-        node.next=self.head
-        self.head=node
-    def inser(self,where,val):
+        if self.head==None:
+            self.head=node
+            return
+        else:
+            node.next=self.head
+            self.head=node
+    def insert(self,where,val):
         node=Node(val)
         if self.head==None:
             self.head=node
@@ -30,14 +35,38 @@ class linkedNode:
                 current=current.next
             node.next=current.next
             current.next=node
-
-
-
-
-
-
-
-
+            return
+    def delete(self,val):
+        if self.head==val:
+            self.head=self.head.next
+        elif self.head==None:
+            return 0
+        else:
+            current=self.head
+            while current.next:
+                if current.next.val==val:
+                    current.next=current.next.next
+                    return
+                else:
+                    current=current.next
+            return -1
+    def display(self):
+        elements=[]
+        if self.head==None:
+            return None
+        current=self.head
+        while current.next:
+            elements.append(str(current.val))
+            current=current.next
+        print('=>'.join(elements))
+if __name__=='__main__':
+    nodelist=Linkednode()
+    for i in range(10):
+        nodelist.append(i)
+    nodelist.preappend(200)
+    nodelist.insert(3,1000)
+    nodelist.delete(8)
+    nodelist.display()
 
 
 
